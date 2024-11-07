08063dcb00ec903e0ddb884934299bc3d0cff8e8
Tagir-Voropaev <tttggg2004@mail.ru>
2024-11-07 17:33:32 +0300

    Completed website design (not adaptive)

--- WeatherApp/weatherapp/main/static/main/js/script.js
+++ WeatherApp/weatherapp/main/static/main/js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    
+
+    var minClick = 2;
+    var click = 0;
+    $( ".scroll-button-left" ).on( "click", function() {
+        
+        if (click >= 1){
+            click-=1;
+            $(".scroll-week").animate({ left: "+=180" }, 200);
+        }
+    } );
+
+    $( ".scroll-button-right" ).on( "click", function() {
+        if (click <= minClick){
+            click+=1;
+            $(".scroll-week").animate({ left: "-=180" }, 200);
+        }
+    } );
 });
