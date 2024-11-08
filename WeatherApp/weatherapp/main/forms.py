3eecbdb16e53a71b4ce7fb844535ce5cea39581c
Tagir-Voropaev <tttggg2004@mail.ru>
2024-11-08 22:40:29 +0300

    Starting a site transplant to postgresql

--- WeatherApp/weatherapp/main/forms.py
+++ WeatherApp/weatherapp/main/forms.py
@@ -1,12 +1,2 @@
-from .models import City
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm
 
-class CityForm(ModelForm):
-    class Meta:
-        model = City
-        fields = ['name']
-        widgets = {'name' : TextInput(attrs={
-            'class' : 'input_form',
-            'id' : 'city',
-            'placeholder' : 'Find a city...'
-            })}
--- WeatherApp/weatherapp/main/views.py
+++ WeatherApp/weatherapp/main/views.py
@@ -1,65 +1,61 @@
 import requests
 from django.shortcuts import render
 import datetime
-from .models import City
-from .forms import CityForm
+import psycopg2
+
+
 
 
 
 def index(request):
+    
     appid = '885bcc371445d48240aa552ca0325b64'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
 
-    if(request.method == 'POST'):
-        form = CityForm(request.POST)   
-        form.save() 
-    form = CityForm()
-
-    cities = City.objects.all()
+   
+       
 
     general_info = {
         'time' : datetime.datetime.today().strftime('%H:%M'),
         'date' : datetime.datetime.today().strftime('%d.%m')
     }
 
-    all_cities = []
+    city = "london"
+    res = requests.get(url.format(city)).json()
 
-    for city in cities:
-        res = requests.get(url.format(city.name)).json()
-        if res.get('main'):
-            city_info = {
-                'city': city.name,
-                'temp' : int(res["main"]["temp"]),
-                'feels_like' : res["main"]["feels_like"],
-                'icon' : res["weather"][0]["icon"],
-                'temp_min' : int(res["main"]["temp_min"]),
-                'temp_max' : int(res["main"]["temp_max"]),
-                'description' : res["weather"][0]["description"],
-                'error': False  
-            }
-        else:
-            city_info = {
-                'city': city.name,
-                'error': True
-            }
-        
-        if (city_info['temp_max'] > 0):
-            city_info['temp_max'] = "+" + str(city_info['temp_max'])
-        elif(city_info['temp_max'] < 0):
-            city_info['temp_max'] = "-" + str(city_info['temp_max'])
-        else:
-            city_info['temp_max'] = "" + str(city_info['temp_max'])
 
-        if (city_info['temp_min'] > 0):
-            city_info['temp_min'] = "+" + str(city_info['temp_min'])
-        elif(city_info['temp_min'] < 0):
-            city_info['temp_min'] = "-" + str(city_info['temp_min'])
-        else:
-            city_info['temp_min'] = "" + str(city_info['temp_min'])
-        all_cities.append(city_info)
-   
+    city_info = {
+        'city': city,
+        'temp' : int(res["main"]["temp"]),
+        'feels_like' : res["main"]["feels_like"],
+        'icon' : res["weather"][0]["icon"],
+        'temp_min' : int(res["main"]["temp_min"]),
+        'temp_max' : int(res["main"]["temp_max"]),
+        'description' : res["weather"][0]["description"],
+        'error': False  
+    }
 
-    return render(request, 'main/index.html', {'all_info' : all_cities, 'general_info' : general_info, 'form' : form})
+
+    
+    
+
+    
+    if (city_info['temp_max'] > 0):
+        city_info['temp_max'] = "+" + str(city_info['temp_max'])
+    elif(city_info['temp_max'] < 0):
+        city_info['temp_max'] = "-" + str(city_info['temp_max'])
+    else:
+        city_info['temp_max'] = "" + str(city_info['temp_max'])
+
+    if (city_info['temp_min'] > 0):
+        city_info['temp_min'] = "+" + str(city_info['temp_min'])
+    elif(city_info['temp_min'] < 0):
+        city_info['temp_min'] = "-" + str(city_info['temp_min'])
+    else:
+        city_info['temp_min'] = "" + str(city_info['temp_min'])
+    
+
+    return render(request, 'main/index.html', {'general_info' : general_info})
 
 
